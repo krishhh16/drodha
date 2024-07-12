92dbd9d4fa55f61f86dd9dae1aa43e881a9a5b71
zeref <krishh16@yahoo.com>
2024-07-12 15:58:25 +0530

    something

---
+++ apps/web/app/trade/[market]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useParams } from 'next/navigation'
+
+function page() {
+    const params = useParams()
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default page
--- apps/web/components/dashboard/TradeMarkets/Market.tsx
+++ apps/web/components/dashboard/TradeMarkets/Market.tsx
@@ -1,12 +1,14 @@
-// components/CryptoTable.js
-import Image from 'next/image'
+'use client'
+import { useRouter } from "next/navigation"
+
 
 const CryptoTable = ({ cryptocurrencies }: any) => {
+  const router = useRouter();
   return (
-    <div className="bg-gray-900  text-white p-4">
+    <div className="bg-[#15141B] rounded-xl text-white p-4">
       <table className="w-full">
         <thead>
-          <tr className="text-gray-400">
+          <tr className="text-gray-400 border-b h-[50px] border-gray-800">
             <th className="text-left">Name</th>
             <th className="text-right">Price</th>
             <th className="text-right">Market Cap</th>
@@ -14,15 +16,15 @@ const CryptoTable = ({ cryptocurrencies }: any) => {
             <th className="text-right">24h Change</th>
             <th className="text-right">24h Price</th>
           </tr>
-        </thead>
+        </thead>  
         <tbody>
           {cryptocurrencies.map((crypto: any) => (
-            <tr key={crypto.id} className="border-b border-gray-800">
+            <tr key={crypto.id}  onClick={() => router.push(`/trade/${crypto.symbol}_USDC`)} className="border-b border-gray-800 cursor-pointer hover:bg-[#45454918] bg-[#15141B]">
               <td className="py-4 flex items-center">
                 <img src={crypto.icon} className="w-8 h-8 object-contain mr-4"  alt={crypto.name} />
                 <div>
-                    <h1>{crypto.name}</h1>
-                    <span className="text-gray-400">{crypto.symbol}</span>
+                    <h1 className="text-sm">{crypto.name}</h1>
+                    <span className="text-gray-400 text-xs">{crypto.symbol}</span>
                 </div>
               </td>
               <td className="text-right">${crypto.price.toFixed(2)}</td>
@@ -31,7 +33,7 @@ const CryptoTable = ({ cryptocurrencies }: any) => {
               <td className={`text-right ${crypto.change >= 0 ? 'text-green-500' : 'text-red-500'}`}>
                 {crypto.change.toFixed(2)}%
               </td>
-              <td className="text-right">
+              <td className="text-right"> 
                 {/* Replace with actual price chart component */}
                 <div className={`h-6 w-20 inline-block ${crypto.change >= 0 ? 'bg-green-500' : 'bg-red-500'}`}></div>
               </td>
