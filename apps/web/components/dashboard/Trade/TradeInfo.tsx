2d06b4ab84acfd305715a46910c8f6a9ff1dcc6f
zeref <krishh16@yahoo.com>
2024-07-13 21:52:19 +0530

    added a bunch of stuff

--- apps/web/components/dashboard/Trade/TradeInfo.tsx
+++ apps/web/components/dashboard/Trade/TradeInfo.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { useParams } from 'next/navigation'
 
 function TradeInfo() {
-    const market = useParams()
+    //@ts-ignore
+    const [base, quote]= useParams().market.split("_")
+
   return (
-    <div onClick={() => console.log(market)} className="w-[80vw] h-full">
-      <div className="h-[10vh] w-full border-y border-slate-400/30">
-        
+    <div onClick={() => console.log(base, quote)} className="w-[80vw] h-full">
+      <div className="h-[10vh] flex items-center justify-left w-full border-y border-slate-400/30">
+            <h1>{base + "/" + quote}</h1>
       </div>
     </div>
   )
 }
 
 export default TradeInfo
- 
