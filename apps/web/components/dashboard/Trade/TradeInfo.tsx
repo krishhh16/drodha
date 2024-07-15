48094849138473f3c5f286d85de4e231334cea2b
zeref <krishh16@yahoo.com>
2024-07-15 12:25:40 +0530

    added orderbook

--- apps/web/components/dashboard/Trade/TradeInfo.tsx
+++ apps/web/components/dashboard/Trade/TradeInfo.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useParams } from 'next/navigation'
+import Orderbook from './orderbook'
 
 function TradeInfo() {
     //@ts-ignore
@@ -23,17 +24,113 @@ function TradeInfo() {
       </div>
       <div className="flex w-full h-[80vh]">
         <div className="w-[80%] h-full">
-          <img className="w-full  object-contain" src="https://img.freepik.com/premium-photo/graph-with-word-graph-showing-word-graph_406811-36121.jpg?w=1380" alt="" />
+          <img className="w-full object-contain" src="https://img.freepik.com/premium-photo/graph-with-word-graph-showing-word-graph_406811-36121.jpg?w=1380" alt="" />
         </div>
         <div className="w-[20%]">
           <div className="flex gap-4">
             <h1 onClick={() => setBookTrade("Book")} className={`font-semibold hover:cursor-pointer transition-all ease-linear duration-75   text-white text-xs ${bookTrade == "Book" && "border-b-2 border-blue-500"} `}>
-              Book              
+              Book
             </h1>
             <h1 onClick={() => setBookTrade("Trade")} className={`font-semibold text-white hover:cursor-pointer transition-all duration-75 ease-linear text-xs ${bookTrade == "Trade" && "border-b-2 border-blue-500"} `}>
-              Trades              
+              Trades
             </h1>
           </div>
+          <div className="bg-black text-white p-4">
+      <table className="w-full">
+        <thead>
+          <tr className="text-xs">
+            <th className="text-left ">Price (USDC)</th>
+            <th className="text-center">Size (BTC)</th>
+            <th className="text-right">Total (BTC)</th>
+          </tr>
+        </thead>
+          <Orderbook
+          data= {[{
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "sell"
+          },
+          {
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "sell"
+          },
+          {
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "sell"
+          },
+          {
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "sell"
+          },
+          {
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "sell"
+          },
+          {
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "sell"
+          },
+          {
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "sell"
+          },
+        
+        ]}
+          />
+
+          <h1 className='text-red-500 text-sm font-bold my-4 text-left'>62,928.5</h1>
+          <Orderbook
+          data= {[{
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "buy"
+          },
+          {
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "buy"
+          },
+          {
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "buy"
+          },
+          {
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "buy"
+          },{
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "buy"
+          },{
+            price: 62989.9,
+            size: 0.45,
+            total: 82932,
+            type: "buy"
+          },
+        ]}
+          />
+      </table>
+    </div>
         </div>
       </div>
     </div>
---
+++ apps/web/components/dashboard/Trade/orderbook.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Orderbook = ({ data }: {data: {price: number, size: number, total: number, type: "sell" | "buy"}[]}) => {
+  return (
+        <tbody>
+          {data.map((order, index) => (
+            <tr key={index} className={"mt-4 " + (order.type === 'sell' ? 'text-red-500' : 'text-green-500')}>
+              <td className="text-left font-extralight text-[0.6vw]">{order.price.toFixed(1)}</td>
+              <td className="text-center text-[0.6vw] font-extralight">{order.size.toFixed(2)}</td>
+              <td className="text-right text-[0.6vw] font-extralight">{order.total.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+  );
+};
+
+export default Orderbook;
